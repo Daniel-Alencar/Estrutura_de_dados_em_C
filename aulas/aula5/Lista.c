98d429f2882ed5d12288d36f282fecedd6b15820
Daniel-Alencar <danielalencar746@gmail.com>
2020-10-01 11:17:02 -0300

    Otimizado algumas coisas e adicionado duas funções: findInList() e isOrdered()

--- aulas/aula5/Lista.c
+++ aulas/aula5/Lista.c
@@ -10,10 +10,13 @@ typedef struct {
 void createList(LIST *list);
 int isEmpty(LIST *list);
 int lengthOfList(LIST *list);
-void insertElement(LIST *list, int position, int value);
+int insertElement(LIST *list, int position, int value);
 int returnElement(LIST *list, int position);
-void deleteElement(LIST *list, int position);
+int deleteElement(LIST *list, int position);
 void printList(LIST *list);
+int findInList(LIST *list, int value);
+int isOrdered(LIST *list);
+void generateList(LIST *list, int firstNumber, int lastNumber);
 
 int main() {
     LIST list;
@@ -42,33 +45,34 @@ void createList(LIST *list) {
 }
 
 int isEmpty(LIST *list) {
-    return list->length == 0;
+    return (!list->length);
 }
 
 int lengthOfList(LIST *list) {
     return list->length;
 }
 
-void insertElement(LIST *list, int position, int value) {
+int insertElement(LIST *list, int position, int value) {
     
     if(lengthOfList(list) == max) {
         printf("\nThere is not store enough in list\n\n");
-        return;
+        return 0;
     }
     if(position < 0 || position > list->length) {
         printf("\nPosition isn't valid\n\n");
-        return;
+        return 0;
     }
 
     if(position != lengthOfList(list)) {
         int i;
-
         for(i=(lengthOfList(list) - 1); i >= position; i--) {
             *(list->values + i + 1) = *(list->values + i);
         }
     }
     *(list->values + position) = value;
     list->length += 1;
+
+    return 1;
 }
 
 int returnElement(LIST *list, int position) {
@@ -80,26 +84,22 @@ int returnElement(LIST *list, int position) {
     return list->values[position];
 }
 
-void deleteElement(LIST *list, int position) {
+int deleteElement(LIST *list, int position) {
 
     if(isEmpty(list)) {
         printf("\nList is empty\n\n");
-        return;
+        return 0;
     }
     if(position < 0 || position >= list->length) {
         printf("\nPosition isn't valid\n\n");
-        return;
+        return 0;
     }
-    if(position == (lengthOfList(list) - 1)) {
-        list->length -= 1;
-    } else {
-        int i;
-
-        for(i=(position + 1); i < lengthOfList(list); i++) {
-            *(list->values + i - 1) = *(list->values + i);
-        }
-        list->length -= 1;
+    
+    int i;
+    for(i=(position + 1); i < lengthOfList(list); i++) {
+        *(list->values + i - 1) = *(list->values + i);
     }
+    list->length -= 1;
 }
 
 void printList(LIST *list) {
@@ -109,4 +109,30 @@ void printList(LIST *list) {
         printf("%i...", list->values[i]);
     }
     printf("\n");
+}
+
+int findInList(LIST *list, int value) {
+    int i;
+    for(i=0; i < lengthOfList(list); i++) {
+        if(*(list->values + i) == value) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int isOrdered(LIST *list) {
+    int i, previous = list->values[0];
+
+    for(i=1; i<lengthOfList(list); i++) {
+        if(list->values[i] < previous) {
+            return 0;
+        }
+        previous = list->values[i];
+    }
+    return 1;
+} 
+
+void generateList(LIST *list, int firstNumber, int lastNumber) {
+    
 }
