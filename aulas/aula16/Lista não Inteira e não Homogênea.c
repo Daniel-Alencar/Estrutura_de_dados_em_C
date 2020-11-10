4db02b1b1a3a60c987820f8ec9c4e8adfd979be3
Daniel-Alencar <danielalencar746@gmail.com>
2020-11-10 19:21:54 -0300

    problema na implementação de belongsToTheList()

--- aulas/aula16/Lista não Inteira e não Homogênea.c
+++ aulas/aula16/Lista não Inteira e não Homogênea.c
@@ -18,9 +18,10 @@ typedef struct nodo {
 typedef NODO *HETEROGENEOUS_LIST;
 
 void createList(HETEROGENEOUS_LIST *list);
-int insertElement(HETEROGENEOUS_LIST *list, int position, int type, union values value);
+void insertElement(HETEROGENEOUS_LIST *list, int position, int type, union values value);
 int lengthOfList(HETEROGENEOUS_LIST list);
 void printList(HETEROGENEOUS_LIST list);
+int belongsToTheList(HETEROGENEOUS_LIST list, int type, union values value);
 
 int main() {
     HETEROGENEOUS_LIST list;
@@ -67,17 +68,21 @@ int lengthOfList(HETEROGENEOUS_LIST list) {
     }
 }
 
-int insertElement(HETEROGENEOUS_LIST *list, int position, int type, union values value) {
+void insertElement(HETEROGENEOUS_LIST *list, int position, int type, union values value) {
     NODO *alocado, *aux;
     int length = lengthOfList(*list);
     if(position < 1 || position > (length + 1)) {
         printf("\nPosition isn't valid\n\n");
         exit(1);
     }
+    if(belongsToTheList(*list, type, value)) {
+        printf("\nThe value already belongs to the list\n\n");
+        exit(2);
+    }
     alocado = (NODO *)malloc(sizeof(NODO));
     if(!alocado) {
         printf("\nUnfortunately, there is not store enough in list\n\n");
-        exit(2);
+        exit(3);
     }
 
     if(type == INTGR) {
@@ -91,9 +96,8 @@ int insertElement(HETEROGENEOUS_LIST *list, int position, int type, union values
         alocado->type = 3;
     } else {
         printf("Error! Type not recognized\n");
-        return 1;
+        exit(4);
     }
-
     if((*list) == NULL) {
         alocado->next = alocado;
         alocado->previous = alocado;
@@ -143,4 +147,31 @@ void printList(HETEROGENEOUS_LIST list) {
     } else {
         printf("[]\n");
     }
+}
+
+int belongsToTheList(HETEROGENEOUS_LIST list, int type, union values value) {
+    NODO *aux = list->next;
+    int retorno = 0;
+    do {
+        if(aux->type == type) {
+            if(type == INTGR) {
+                if(value.intValue == aux->VALUE.intValue) {
+                    retorno = 1;
+                    break;
+                }
+            } else if(type == FLT) {
+                if(value.floatValue == aux->VALUE.floatValue) {
+                    retorno = 1;
+                    break;
+                }
+            } else if(type == STRING) {
+                if(strcmp(value.stringValue, aux->VALUE.stringValue) == 0) {
+                    retorno = 1;
+                    break;
+                }
+            }
+        }
+        aux = aux->next;
+    } while(aux != list->next);
+    return retorno;
 }
